481c23d7b638008598e92a5ce27aa4b04a4c2514
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-10-04 01:32:05 +0900

    新規投稿とTLの実装

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,19 +10,22 @@ class ActivitiesController < ApplicationController
 
   def create
     @activity = Activity.new(activity_params)
-    binding.pry
     if @activity.save
       redirect_to action: :create
     else
-      render :new
+      render root_path
     end
+  end
+
+  def show
+    @activity = Activity.find(params[:id])
   end
 
 
   private
 
   def activity_params
-    params.require(:activity).permit(:group, :activity_day, :song, :studio, :text, :image).merge(user_id: current_user.id)
+    params.require(:activity).permit(:group, :activity_day, :song, :studio, :text, :image).merge(user: current_user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
-  resources :activities, only: [:index, :new, :create, :show]
+  resources :activities
   resources :users, only: [:index, :show]
 end
