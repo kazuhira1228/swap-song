0b7f61b8cf1a0b43b11feb047ccf7aa61fed2c5f
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-10-03 17:28:57 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :password,      format:       { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :sex_id,        numericality: { other_then: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_then: 1, message: "can't be blank" }
+    validates :city
+    validates :holiday_id,    numericality: { other_then: 1, message: "can't be blank" }
+    validates :member_since
+    validates :favorite_part
+    validates :group
+    validates :more_join_id,  numericality: { other_then: 1, message: "can't be blank" }
+  end
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sex
