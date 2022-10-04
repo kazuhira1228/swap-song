04693139c21e8e8dc47786332e7a2bad855dd16a
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-10-04 12:53:52 +0900

    新規投稿の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,24 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:group, :first_user, :second_user, :third_user, :fourth_user, :fifth_user, :sixth_user, :group_since, :text).merge(user_id: current_user.id)
+  end
 end
