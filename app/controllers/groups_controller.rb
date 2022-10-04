f5306ec14cee80fb67e53b148fcdaedd72a78040
kazzhrsw <kazuhira_hrsw@icloud.com>
2022-10-04 13:20:43 +0900

    編集・削除機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,24 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    group = Group.find(params[:id])
+    group.destroy
+    redirect_to action: :index
+  end
 
   private
 
